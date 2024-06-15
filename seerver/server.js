3c74733d1788d0e7ebd130ef04ddab1340c13f00
Utkarsh Raj <uraj33175@gmail.com>
2024-06-15 12:29:15 +0530

    new

--- seerver/server.js
+++ seerver/server.js
@@ -10,10 +10,27 @@ app.use(cors());
 
 app.get('/api/inventory', (req, res) => {
     const results = [];
+    const { brand, product_type } = req.query;
+
+    
+
     fs.createReadStream(path.join(__dirname, 'sample-data-v2.csv'))
         .pipe(csv())
-        .on('data', (data) => results.push(data))
+        .on('data', (data) => {
+            
+            let include = true;
+            if (brand && data.brand !== brand) {
+                include = false;
+            }
+            if (product_type && data.product_type !== product_type) {
+                include = false;
+            }
+            if (include) {
+                results.push(data);
+            }
+        })
         .on('end', () => {
+           
             res.json(results);
         });
 });
